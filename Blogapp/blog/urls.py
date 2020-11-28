b4f2488e4158768c4533cf949741b7ac7f7a4f3b
Md-Borhan-Uddin <mdborhanuddinab21@gmail.com>
2020-11-28 12:47:00 +0600

    add single post functionality and some design

--- Blogapp/blog/urls.py
+++ Blogapp/blog/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
-    path('single', views.single, name='single'),
+    path('single/<int:id>', views.single, name='single'),
     path('login', views.login, name='login'),
+    path('author', views.author, name='author'),
+    path('category', views.category, name='category'),
 ]
--- Blogapp/blog/views.py
+++ Blogapp/blog/views.py
@@ -16,9 +16,26 @@ def contact(request):
 
 
 
-def single(request):
-    return render(request, 'blog/single.html')
+def single(request, id):
+    post =  Post.objects.get(id=id)
+    author = Author.objects.get(id=post.post_author.id)
+    category = Category.objects.get(name=post.post_category)
+    related_cat = Post.objects.filter(post_category=category)
+    top_posts = Post.objects.filter(post_author=author)[:2]
+    context = {
+        'post':post,
+        'top_posts':top_posts,
+        'cats':related_cat,
+        'category':category
+    }
+    return render(request, 'blog/single.html', context)
 
+
+def author(request):
+    return render(request, 'blog/author.html')
+
+def category(request):
+    return render(request, 'blog/category.html')
 
 def login(request):
     return render(request, 'blog/login.html')
